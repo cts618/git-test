745d9090fca90a34fb784887a254f59a38b57ff6
wanglin <1312993017@qq.com>
2024-05-01 16:02:49 +0800

    third commit

--- GitTest.py
+++ GitTest.py
@@ -1,2 +1,10 @@
-print("hello git,second commit\n")
-print("hello git,third commit\n")
+from functools import reduce
+
+def is_not(x,y):
+    print("hello, git1!\n")
+    print("hello, git2!\n")
+    print("hello, git3!\n")
+    # print("hello, git new branch\n")
+    # print("hello, git new branch\n")
+
+    pass
